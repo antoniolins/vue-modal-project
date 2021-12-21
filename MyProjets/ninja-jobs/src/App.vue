60ca58aa00cfe9b57b03fe841ef5ef68001503e1
antoniolins <antoniolins@uol.com.br>
2021-12-21 16:09:08 -0300

    comment

---
+++ MyProjets/ninja-jobs/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="App">
+      <p>{{name}} - {{age}}</p>
+      <button @click="changeName('Zelda')">Change Name</button>
+       <button @click="changeAge(30)">Change Age</button>
+  </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, ref, toRefs } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {},
+  setup () {
+    /*     const state = reactive({
+      name: 'Link',
+      age: 27 as number | string
+    })
+
+    state.name = 'Antonio'
+
+    return { ...toRefs(state) }
+*/
+    const name = ref('Lins')
+    const age = ref<number | string>(25)
+
+    return { name, age }
+  },
+  methods: {
+    changeName (name: string) {
+      this.name = name
+      return name
+    },
+    changeAge (age: number |string) {
+      this.age = age
+      return age
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
---
+++ MyProjets/ninja-jobs/src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>"Welcome to Your Vue.js + TypeScript App"</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+
+  }
+})
+</script>
